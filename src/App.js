ed7fd4d5a358e8e23796854c3f3952faa4f7a331
Your Full Name <harshajami2005@gmail.com>
2025-08-06 18:06:39 +0530

    removed imports

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/home";
-import About from "./components/about";
-import Login from "./dashboard/login";
-import Signup from "./dashboard/signup"; 
-import UsersDashboard from "./dashboard/usersdashboard"; 
-import OAuthCallback from "./dashboard/Ouath"; 
 import DuplicateDashboard from "./dashboard/duplicatedashboard";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/services" element={<About />} />
+         <Route path="/" element={<Home />} />
+        {/*
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/signup" element={<Signup />} /> */}
         <Route path="/userdash" element={<UsersDashboard />} /> {/* Corrected to uppercase */}
-        <Route path="/oauth-callback" element={<OAuthCallback />} />
+        {/* <Route path="/oauth-callback" element={<OAuthCallback />} /> */}
         <Route path="/duplicate-dashboard" element={<DuplicateDashboard />} />
       </Routes>
     </Router>
